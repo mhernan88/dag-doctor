0c0ed6879168a0619e38e0e5af7dc83dc81bd250
Michael Hernandez <mhernan88@gmail.com>
2023-08-16 17:44:26 -0500

    added csv loader

---
+++ dag/file_test.go
@@ -0,0 +1,18 @@
+package dag
+
+import (
+    "testing"
+)
+
+func TestPipelineFile__LoadAndDisplay(t *testing.T) {
+    pf := PipelineFile{
+        Name: "test",
+        Filename: "../test_file.csv",
+    }
+
+    err := pf.LoadAndDisplay(99)
+    if err != nil {
+        t.Error(err)
+    }
+}
+
