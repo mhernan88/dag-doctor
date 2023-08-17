b35e9ae6fabc77d989ec2f86bfc77e1214e21b98
Michael Hernandez <mhernan88@gmail.com>
2023-08-17 01:02:20 -0500

    split load into pipeline and catalog

--- dag/load.go
+++ dag/load.go
@@ -7,7 +7,7 @@ import (
     "encoding/json"
 )
 
-func Load(filename string) (*Pipeline, error) {
+func LoadPipeline(filename string) (*Pipeline, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open file: %v", err)
@@ -25,4 +25,24 @@ func Load(filename string) (*Pipeline, error) {
 	}
 
 	return &pipeline, nil
+}
+
+func LoadCatalog(filename string) (*map[string]PipelineFile, error) {
+    file, err := os.Open(filename)
+    if err != nil {
+        return nil, fmt.Errorf("failed to open file: %v", err)
+    }
+    defer file.Close()
+
+    bytes, err := io.ReadAll(file)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read file: %v", err)
+    }
+
+    var catalog PipelineFileContainer
+    if err := json.Unmarshal(bytes, &catalog); err != nil {
+        return nil, fmt.Errorf("failed to unmarshal JSON: %v", err)
+    }
+
+    return &catalog.Catalog, nil
 }
