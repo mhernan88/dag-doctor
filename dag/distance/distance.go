daf714bce1f43f41ee624ad215bad37f614e507a
Michael Hernandez <mhernan88@gmail.com>
2023-08-20 21:31:47 -0500

    got rid of struct

--- dag/distance/distance.go
+++ dag/distance/distance.go
@@ -3,30 +3,24 @@ package distance
 import (
     "errors"
     "github.com/sirupsen/logrus"
+    "github.com/mhernan88/dag-bisect/dag
 )
 
-func NewDistanceCalculator(l *logrus.Logger) *DistanceCalculator {
-    return &DistanceCalculator{l}
-}
 
-type DistanceCalculator struct {
-    l *logrus.Logger
-}
-
-func (dc DistanceCalculator) ComputeSplitCandidates(roots []*Node, limit int) ([]string, error) {
+func ComputeSplitCandidates(roots []*dag.Node, limit int, l *logrus.Logger) ([]string, error) {
     var midpoints []string
 
     var nd *Node
     for len(roots) > 0 {
         nd, roots = roots[[len(roots)-1], roots[:len(roots)-1]
-        dc.l.Tracef("popped node %s from queue", nd.Name)
+        l.Tracef("popped node %s from queue", nd.Name)
 
-        distanceToStart, err := dc.DistanceToStart(nd, limit)
+        distanceToStart, err := distanceToStart(nd, limit)
         if err != nil {
             return nil, err
         }
 
-        distanceToEnd, err := dc.DistanceToEnd(nd, limit)
+        distanceToEnd, err := distanceToEnd(nd, limit)
         if err != nil {
             return nil, err
         }
@@ -43,25 +37,25 @@ func (dc DistanceCalculator) ComputeSplitCandidates(roots []*Node, limit int) ([
 }
 
 // DistanceToStart calculates the longest path to the start of the dag.
-func (dc DistanceCalculator) DistanceToStart(node *Node, limit int) (int, error) {
+func distanceToStart(node *dag.Node, limit int, l *logrus.Logger) (int, error) {
     dc.l.Trace("calculating DistanceToStart()")
 
     // Edge cases where node is leaf node.
     if (node.Prev == nil) || (len(node.Prev) == 0) {
-        dc.l.Tracef("node %s has no parents, DistanceToStart() is 0", node.Name)
+        l.Tracef("node %s has no parents, DistanceToStart() is 0", node.Name)
         return 0, nil
     }
 
     // Go Eqvuialent of Tuple (Node, int)
-    var nodes []NodeWrapper
-    var nd NodeWrapper
+    var nodes []dag.NodeWrapper
+    var nd dag.NodeWrapper
 
     // Assemble a queue of NodeWrappers
     for _, nodePtr := range node.Prev {
         if nodePtr == nil {
             continue
         }
-        nodes = append(nodes, NodeWrapper{Node: *nodePtr, Distance: 1})
+        nodes = append(nodes, dag.NodeWrapper{Node: *nodePtr, Distance: 1})
     }
 
     maxDistance := 1
@@ -70,14 +64,14 @@ func (dc DistanceCalculator) DistanceToStart(node *Node, limit int) (int, error)
     // Queue To Iterate Over All Cases
     for len(nodes) > 0 {
         nd, nodes = nodes[len(nodes)-1], nodes[:len(nodes)-1]
-        dc.l.Tracef("popped node %s from queue", nd.Node.Name)
+        l.Tracef("popped node %s from queue", nd.Node.Name)
 
         for _, childNode := range nd.Node.Prev {
-            dc.l.Tracef("analyzing childNode %s", childNode.Name)
+            l.Tracef("analyzing childNode %s", childNode.Name)
             maxDistance = max(nd.Distance + 1, maxDistance)
-            childNodeWrapper := NodeWrapper{Node: *childNode, Distance: nd.Distance + 1}
+            childNodeWrapper := dag.NodeWrapper{Node: *childNode, Distance: nd.Distance + 1}
             nodes = append(nodes, childNodeWrapper)
-            dc.l.Tracef("calculated node %s distance and added it to queue", childNode.Name)
+            l.Tracef("calculated node %s distance and added it to queue", childNode.Name)
         }
 
         i++
@@ -88,30 +82,30 @@ func (dc DistanceCalculator) DistanceToStart(node *Node, limit int) (int, error)
         }
     }
 
-    dc.l.Tracef("node %s has DistanceToStart() of %d", node.Name, maxDistance)
+    l.Tracef("node %s has DistanceToStart() of %d", node.Name, maxDistance)
     return maxDistance, nil
 }
 
 // DistanceToEnd calculates the longest path to the end of the dag.
-func (dc DistanceCalculator) DistanceToEnd(node *Node, limit int) (int, error) {
-    dc.l.Trace("calculating DistanceToEnd()")
+func distanceToEnd(node *dag.Node, limit int) (int, error) {
+    l.Trace("calculating DistanceToEnd()")
 
     // Edge cases where node is leaf node.
     if (node.Next == nil) || (len(node.Next) == 0) {
-        dc.l.Tracef("node %s has no children, DistanceToEnd() is 0", node.Name)
+        l.Tracef("node %s has no children, DistanceToEnd() is 0", node.Name)
         return 0, nil
     }
 
     // Go Eqvuialent of Tuple (Node, int)
-    var nodes []NodeWrapper
-    var nd NodeWrapper
+    var nodes []dag.NodeWrapper
+    var nd dag.NodeWrapper
 
     // Assemble a queue of NodeWrappers
     for _, nodePtr := range node.Next {
         if nodePtr == nil {
             continue
         }
-        nodes = append(nodes, NodeWrapper{Node: *nodePtr, Distance: 1})
+        nodes = append(nodes, dag.NodeWrapper{Node: *nodePtr, Distance: 1})
     }
 
     maxDistance := 1
@@ -120,14 +114,14 @@ func (dc DistanceCalculator) DistanceToEnd(node *Node, limit int) (int, error) {
     // Queue To Iterate Over All Cases
     for len(nodes) > 0 {
         nd, nodes = nodes[len(nodes)-1], nodes[:len(nodes)-1]
-        dc.l.Tracef("popped node %s from queue", nd.Node.Name)
+        l.Tracef("popped node %s from queue", nd.Node.Name)
 
         for _, childNode := range nd.Node.Next {
-            dc.l.Tracef("analyzing childNode %s", childNode.Name)
+            l.Tracef("analyzing childNode %s", childNode.Name)
             maxDistance = max(nd.Distance + 1, maxDistance)
-            childNodeWrapper := NodeWrapper{Node: *childNode, Distance: nd.Distance + 1}
+            childNodeWrapper := dag.NodeWrapper{Node: *childNode, Distance: nd.Distance + 1}
             nodes = append(nodes, childNodeWrapper)
-            dc.l.Tracef("calculated node %s distance and added it to queue", nd.Node.Name)
+            l.Tracef("calculated node %s distance and added it to queue", nd.Node.Name)
         }
 
         i++
@@ -138,6 +132,6 @@ func (dc DistanceCalculator) DistanceToEnd(node *Node, limit int) (int, error) {
         }
     }
 
-    dc.l.Tracef("node %s has DistanceToEnd() of %d", node.Name, maxDistance)
+    l.Tracef("node %s has DistanceToEnd() of %d", node.Name, maxDistance)
     return maxDistance, nil
 }
