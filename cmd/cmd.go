33fa9ab2ba0c1bff51754f83715c32a6e67902d1
Michael Hernandez <mhernan88@gmail.com>
2023-08-22 12:51:49 -0500

    fixed import

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,14 +1,32 @@
 package cmd
 
 import (
-    "github.com/dag-bisect/data
+    "github.com/mhernan88/dag-bisect/data"
+    "github.com/sirupsen/logrus"
 )
+
+func NewUI(
+    nodes map[string]*data.Node,
+    catalog map[string]data.Dataset,
+    l *logrus.Logger,
+) UI {
+    return UI{
+        nodes: nodes,
+        okNodes: make(map[string]*data.Node),
+        errNodes: make(map[string]*data.Node),
+        catalog: catalog,
+        l: l,
+    }
+}
 
 type UI struct {
     nodes map[string]*data.Node
     okNodes map[string]*data.Node
     errNodes map[string]*data.Node
+    catalog map[string]data.Dataset
+    l *logrus.Logger
 }
 
-func Run(l *logrus.Logger) {
+func (ui UI) Run() {
+    ui.l.Debug("running ui loop")
 }
