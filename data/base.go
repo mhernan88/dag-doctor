c508e286f37b4d766e1edee145c5d9dfccbcd141
Michael Hernandez <mhernan88@gmail.com>
2023-08-24 16:51:54 -0500

    added dag data struct

--- data/base.go
+++ data/base.go
@@ -10,6 +10,28 @@ type Node struct {
 	Prev             map[string]*Node `json:"-"` // map[nodeName]nodePointer
 }
 
+type DAG struct {
+    Nodes map[string]*Node
+}
+
+func NewDAG(nodes []*Node) DAG {
+    dag := make(map[string]*Node)
+    for _, node := range nodes {
+        dag[node.Name] = node
+    }
+    return DAG{Nodes: dag}
+}
+
+func (d DAG) ToSlices() ([]string, []*Node){
+    var names []string
+    var nodes []*Node
+    for name, node := range d.Nodes {
+        names = append(names, name)
+        nodes = append(nodes, node)
+    }
+    return names, nodes
+}
+
 type Dataset struct {
     Filename string `json:"filename"`
 }
