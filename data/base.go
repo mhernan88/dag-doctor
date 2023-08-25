ed5b4d958c03c9d5946ca2160352b7dd8d29d213
Michael Hernandez <mhernan88@gmail.com>
2023-08-24 23:17:57 -0500

    added state for cyclicality

--- data/base.go
+++ data/base.go
@@ -1,5 +1,9 @@
 package data
 
+type Dataset struct {
+    Filename string `json:"filename"`
+}
+
 type Node struct {
 	Name             string          `json:"name"`
 	Inputs           []string        `json:"inputs"`
@@ -8,31 +12,62 @@ type Node struct {
     PrevLabels []string `json:"prev"` // []nodeName
 	Next             map[string]*Node `json:"-"` // map[nodeName]nodePointer
 	Prev             map[string]*Node `json:"-"` // map[nodeName]nodePointer
+    State string `json:"-"`
 }
 
-type DAG struct {
-    Nodes map[string]*Node
-}
-
-func NewDAG(nodes []*Node) DAG {
+func NewNodeMap(nodeslice []*Node) map[string]*Node {
     dag := make(map[string]*Node)
-    for _, node := range nodes {
+    for _, node := range nodeslice {
         dag[node.Name] = node
     }
-    return DAG{Nodes: dag}
+    return dag
 }
 
-func (d DAG) ToSlices() ([]string, []*Node){
+func SliceNodeMap(nodemap map[string]*Node) ([]string, []*Node) {
     var names []string
     var nodes []*Node
-    for name, node := range d.Nodes {
+    for name, node := range nodemap {
         names = append(names, name)
         nodes = append(nodes, node)
     }
     return names, nodes
 }
 
-type Dataset struct {
-    Filename string `json:"filename"`
+func GetNodeDescendants(sources []*Node) map[string]*Node {
+    var nodes []*Node
+    for _, source := range sources {
+        nodes = append(nodes, source)
+    }
+
+    descendants := make(map[string]*Node)
+    for len(nodes) > 0 {
+        node := nodes[len(nodes)-1]
+        nodes := nodes[:len(nodes)-1]
+
+        for childName, child := range node.Next {
+            descendants[childName] = child
+            nodes = append(nodes, child)
+        }
+    }
+    return descendants
 }
 
+func GetNodeAncestors(sources []*Node) map[string]*Node {
+    var nodes []*Node
+    for _, source := range sources {
+        nodes = append(nodes, source)
+    }
+
+    ancestors := make(map[string]*Node)
+
+    for len(nodes) > 0 {
+        node := nodes[len(nodes)-1]
+        nodes = nodes[:len(nodes)-1]
+
+        for childName, child := range node.Prev {
+            ancestors[childName] = child
+            nodes = append(nodes, child)
+        }
+    }
+    return ancestors
+}
