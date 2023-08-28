774c382c7facc28fe06f40e5f5b9eccd0171d74c
Michael Hernandez <mhernan88@gmail.com>
2023-08-27 21:56:42 -0500

    dag stuff

--- cmd/dag_loop.go
+++ cmd/dag_loop.go
@@ -1,49 +1,58 @@
 package cmd
 
 import (
-    "fmt"
-    "github.com/enescakir/emoji"
+	"fmt"
+
+	"github.com/enescakir/emoji"
 )
 
 func (ui *UI) CheckDAG() error {
-    fmt.Println("inspecting DAG")
+	fmt.Println("inspecting DAG")
 
-    dagOK := true
+	dagOK := true
 
-    for len(ui.nodes) > 0 {
-        node, err := ui.splitter.FindCandidate(ui.nodes)
-        if err != nil {
-            return err
-        }
+	for len(ui.nodes) > 0 {
+		node, err := ui.splitter.FindCandidate(ui.nodes)
+		if err != nil {
+			return err
+		}
 
-        ui.l.Tracef("selected split candidate: %s", node.Name)
+		ui.l.Tracef("selected split candidate: %s", node.Name)
 
-        ok, prunedNodes, err := ui.CheckNode(node)
-        ui.l.Tracef("prunedNodes = %v", prunedNodes)
-        if err != nil {
-            return err
-        }
+		ok, prunedNodes, err := ui.CheckNode(node)
+		ui.l.Tracef("prunedNodes = %v", prunedNodes)
+		if err != nil {
+			return err
+		}
 
-        if !ok {
-            dagOK = false
-        }
+		if !ok {
+			dagOK = false
+		}
 
-        if len(prunedNodes) == 0 {
-            if dagOK {
-                fmt.Printf(
-                    "%v dag ok\n",
-                    emoji.GrinningFace,
-                )
-                return nil
-            } else {
-                fmt.Printf(
-                    "%v source of error: '%s'\n",
-                    emoji.Skull,
-                    node.Name,
-                )
-                return nil
-            }
-        }
-    }
-    return nil
+		if !dagOK && (node.Next == nil) {
+			fmt.Printf(
+				"%v source of error: '%s'\n",
+				emoji.Skull,
+				node.Name,
+			)
+		}
+
+		if len(prunedNodes) == 0 {
+			if dagOK {
+				fmt.Printf(
+					"%v dag ok\n",
+					emoji.GrinningFace,
+				)
+				return nil
+			} else {
+				fmt.Printf(
+					"%v source of error: '%s'\n",
+					emoji.Skull,
+					node.Name,
+				)
+				return nil
+			}
+		}
+	}
+	return nil
 }
