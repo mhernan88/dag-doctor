008d6fcfd55585ceb22d5e06342e076a9fd12e6c
= <=>
2023-09-14 03:58:49 +0000

    added json saving

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/mhernan88/dag-bisect/data"
 	"github.com/mhernan88/dag-bisect/pruners"
 	"github.com/mhernan88/dag-bisect/splitters"
@@ -15,34 +18,55 @@ func NewUI(
 	l *logrus.Logger,
 ) UI {
 	return UI{
-		dag:            dag,
-		okNodes:        make(map[string]data.Node),
-		errNodes:       make(map[string]data.Node),
-		lastFailedNode: "",
-		splitter:       splitter,
-		pruner:         pruner,
-		iterationLimit: iterationLimit,
-		l:              l,
+		DAG:            dag,
+		OKNodes:        make(map[string]data.Node),
+		ERRNodes:       make(map[string]data.Node),
+		LastFailedNode: "",
+		Splitter:       splitter,
+		Pruner:         pruner,
 	}
 }
 
-type UI struct {
-	dag            data.DAG
-	okNodes        map[string]data.Node
-	errNodes       map[string]data.Node
-	lastFailedNode string
-	splitter       splitters.Splitter
-	pruner         pruners.Pruner
-	iterationLimit int
-	l              *logrus.Logger
+func SaveState(filename string, ui UI) error {
+	f, err := os.OpenFile(filename, os.O_CREATE | os.O_APPEND | os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	encoder := json.NewEncoder(f)
+	encoder.Encode(ui)
+	return nil
 }
 
-func (ui *UI) Run() error {
-	ui.l.Debug("running ui loop")
+func LoadState(filename string) (*UI, error) {
+	f, err := os.OpenFile(filename, os.O_RDONLY, 0666)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var ui UI
+	decoder := json.NewDecoder(f)
+	decoder.Decode(&ui)
+	return &ui, nil
+}
+
+type UI struct {
+	DAG data.DAG `json:"dag"`
+	OKNodes        map[string]data.Node `json:"ok_nodes"`
+	ERRNodes       map[string]data.Node `json:"err_nodes"`
+	LastFailedNode string `json:"last_failed_node"`
+	Splitter       splitters.Splitter `json:"splitter"`
+	Pruner         pruners.Pruner `json:"pruner"`
+}
+
+func (ui *UI) Run(l *logrus.Logger) error {
+	l.Debug("running ui loop")
 	err := ui.CheckDAG()
 	if err != nil {
 		return err
 	}
-	ui.l.Debug("terminating ui")
+	l.Debug("terminating ui")
 	return nil
 }
