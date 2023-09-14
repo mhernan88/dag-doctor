cc3af8bad4aa0f9fa31752da58638443445a85ad
= <=>
2023-09-14 03:26:31 +0000

    created some static-ish folders

---
+++ shared/constants.go
@@ -0,0 +1,38 @@
+package shared
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func GetConfigFolder() (string, error) {
+	homeFolder, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(homeFolder, ".config", "dag_doctor"), nil
+}
+
+func GetLogFilename() (string, error) {
+	configFolder, err := GetConfigFolder()
+	if err != nil {
+		return "", nil
+	}
+	return filepath.Join(configFolder, "log.json"), nil
+}
+
+func GetOptFilename() (string, error) {
+	configFolder, err := GetConfigFolder()
+	if err != nil {
+		return "", nil
+	}
+	return filepath.Join(configFolder, "opt.json"), nil
+}
+
+func GetDBFilename() (string, error) {
+	configFolder, err := GetConfigFolder()
+	if err != nil {
+		return "", nil
+	}
+	return filepath.Join(configFolder, "data.db"), nil
+}
