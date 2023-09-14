d99ad3ee7552b92af78d7132c2a2439f9c3975cb
= <=>
2023-09-14 03:58:59 +0000

    new routes for sessions

---
+++ cmd/sessions/new.go
@@ -0,0 +1,14 @@
+package sessions
+
+// import (
+// 	"github.com/mhernan88/dag-bisect/data"
+// 	"github.com/urfave/cli/v2"
+// )
+//
+// func newSession(ctx * cli.Context) error {
+// 	dag, err := data.LoadDAG(ctx.Args().Get(0))
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
