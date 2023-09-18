4a06e59bf80611d5e2898087bdeee45fe34a611d
Michael Hernandez <mhernan88@gmail.com>
2023-09-18 17:49:19 -0500

    moving to global session mgr?

--- cmd/sessions/base.go
+++ cmd/sessions/base.go
@@ -1,22 +1,31 @@
 package sessions
 
-import "github.com/urfave/cli/v2"
+import (
+	"log/slog"
+	"os"
 
-var SessionsDevCmd = cli.Command{
-	Name: "dev",
-	Usage: "session dev commands",
-	Subcommands: []*cli.Command{
-		&UpdateSessionCmd,
-	},
+	"github.com/jmoiron/sqlx"
+	"github.com/mhernan88/dag-bisect/db"
+	"github.com/mhernan88/dag-bisect/shared"
+)
+
+
+func NewSessionManager(cxn *sqlx.DB, l *slog.Logger, f *os.File) SessionManager {
+	return SessionManager{cxn: cxn, l: l, f: f}
 }
 
-var SessionsCmd = cli.Command{
-	Name: "session",
-	Usage: "session commands",
-	Subcommands: []*cli.Command{
-		&ListSessionsCmd,
-		&NewSessionCmd,
-		&SessionsDevCmd,
-	},
+func NewDefaultSessionManager() (*SessionManager, error) {
+	l, f := shared.GetLogger()
+	cxn, err := db.Connect()
+	if err != nil {
+		return nil, err
+	}
+	sm := NewSessionManager(cxn, l, f)
+	return &sm, nil
 }
 
+type SessionManager struct {
+	cxn *sqlx.DB
+	l *slog.Logger
+	f *os.File
+}
--- cmd/sessions/iter.go
+++ cmd/sessions/iter.go
@@ -1,2 +1,24 @@
 package sessions
 
+import (
+	"log/slog"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/urfave/cli/v2"
+)
+
+func NewIterSessionManager(cxn sqlx.DB, l *slog.Logger) IterSessionManager {
+	return IterSessionManager{
+		cxn: cxn,
+		l: l,
+	}
+}
+
+type IterSessionManager struct {
+	cxn sqlx.DB
+	l *slog.Logger
+}
+
+func iterSession(ctx *cli.Context) error {
+
+}
